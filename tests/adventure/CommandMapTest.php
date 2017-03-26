33defb105e61d277d725fa3f55c47766eb91f6cf
Jorge Tite <jorgetite@me.com>
2017-03-26 11:10:13 -0400

    Removed unwanted files

---
+++ tests/adventure/CommandMapTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jorgetite
+ * Date: 3/26/17
+ * Time: 10:20 AM
+ */
+
+namespace adventure;
+
+use Adventure\CommandMap;
+use PHPUnit\Framework\TestCase;
+
+class CommandMapTest extends TestCase
+{
+
+}
