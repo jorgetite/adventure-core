15bac39f48bb4bf0663ec10b31e0b948f31a783b
Jorge Tite <jorgetite@me.com>
2017-03-25 23:06:02 -0400

    Defined command abstractions

--- src/adventure/Command.php
+++ src/adventure/Command.php
@@ -8,13 +8,52 @@
 namespace Adventure;
 
 /**
+ * The Command interface defines methods to execute commands entered by a player
+ * of an adventure game. A Command can receive arguments that will be processed
+ * by each concrete implementation.
+ *
+ * This class is based on the "World of Zuul" framework for writing very
+ * simple text-based adventure games.
  *
  * @author    jorgetite
- * @since     3/16/17
+ * @since     3/1
  * @version   1.0
  * @copyright Copyright (c) 2017 Jorge Tite
  */
 interface Command
 {
-    public function execute() : void;
+    /**
+     * Executes this Command and returns the result.
+     *
+     * @param Character $character
+     *        the character in the adventure game
+     *
+     * @return string
+     *         the result of the Command
+     */
+    public function execute(Character $character) : string;
+
+    /**
+     * Sets the arguments for this Command.
+     *
+     * @param string $args
+     *        the arguments for this Command
+     */
+    public function setArguments(string $args) : void;
+
+    /**
+     * Returns the arguments for this Command.
+     *
+     * @return string
+     *         the arguments for this Command
+     */
+    public function getArguments() : string;
+
+    /**
+     * Checks whether this command has arguments set or not.
+     *
+     * @return bool
+     *         true if the Command has arguments, false otherwise.
+     */
+    public function hasArguments() : bool;
 }
--- src/adventure/Command/AbstractCommand.php
+++ src/adventure/Command/AbstractCommand.php
@@ -1,15 +1,73 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: jorgetite
- * Date: 3/25/17
- * Time: 8:55 PM
+/*
+ * Copyright © 2017 Jorge Tite
+ *
+ * Please review the LICENSE file distributed with this software for full
+ * copyright information.
  */
-
 namespace Adventure\Command;
 
+/**
+ * A generic implementation of the Command interface. Subclasses should be
+ * extended to implement concrete commands in the adventure game.
+ *
+ * <p>This class is based on the "World of Zuul" framework for writing very
+ * simple text-based adventure games.
+ *
+ * @author    jorgetite
+ * @since     3/1
+ * @version   1.0
+ * @copyright Copyright (c) 2017 Jorge Tite
+ */
 
-class AbstractCommand
+abstract class AbstractCommand
 {
+    /**
+     * @var string
+     *      this command arguments
+     */
+    private $args;
 
+    /**
+     * Creates a new instance of a command with no arguments.
+     */
+    public function __construct()
+    {
+        $this->args = "";
+    }
+
+    /**
+     * Sets the arguments for this command. Arguments are words following the
+     * command name. An empty string should be used to indicate that the command
+     * does not have arguments.
+     *
+     * @param string $args
+     *        the arguments for this Command
+     */
+    public function setArguments(string $args) : void
+    {
+        $this->args = $args;
+    }
+
+    /**
+     * Returns the last arguments for this command.
+     *
+     * @return string
+     *         the arguments for this Command
+     */
+    public function getArguments() : string
+    {
+        return $this->args;
+    }
+
+    /**
+     * Checks whether this command has arguments set or not.
+     *
+     * @return bool
+     *         true if the Command has arguments, false otherwise.
+     */
+    public function hasArguments() : bool
+    {
+        return !empty($this->args);
+    }
 }
