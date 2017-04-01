1c07bad735006aaec751a66e81e9d3d8fbc7ce2b
Jorge Tite <jorgetite@me.com>
2017-03-31 21:00:20 -0400

    Implemented Parser class

--- src/adventure/Parser.php
+++ src/adventure/Parser.php
@@ -6,8 +6,11 @@
  * copyright information.
  */
 namespace Adventure;
+use Ds\Set;
 
 /**
+ * The Parser class is responsible for processing the player's input and match
+ * the command to be executed.
  *
  * @author    jorgetite
  * @since     3/16/17
@@ -16,5 +19,59 @@ namespace Adventure;
  */
 class Parser
 {
+    /**
+     * @var CommandMap
+     *      the command words and respective commands
+     */
+    private $commands;
 
+    /**
+     * Creates a new instance or Parser using the commands map defined for the
+     * adventure game.
+     *
+     * @param CommandMap $commands
+     *        the commands for the adventure game
+     */
+    public function __construct(CommandMap $commands)
+    {
+        $this->commands = $commands;
+    }
+
+    /**
+     * Parses the input and tries to match it to one of the commands defined for
+     * the adventure game. This method uses the first word in the player's input
+     * to find a matching command word.
+     *
+     * @param string $input
+     *        the player's input
+     *
+     * @return Command|null
+     */
+    public function matchCommand(string $input) : ?Command
+    {
+        $command = null;
+
+        if (!empty($input)) {
+            // gets the first word
+            $words = explode(" ", $input, 2);
+            $command = $this->commands->getCommand($words[0]);
+
+            if ($command != null && !empty($words[1])) {
+                $command->setArguments($words[1]);
+            }
+        }
+
+        return $command;
+    }
+
+    /**
+     * Returns the command words recognized by this parser.
+     *
+     * @return Set
+     *         a collection of command words
+     */
+    public function getCommandWords() : Set
+    {
+        return $this->commands->getCommandWords();
+    }
 }
---
+++ tests/adventure/ParserTest.php
@@ -0,0 +1,108 @@
+<?php
+/*
+ * Copyright © 2017 Jorge Tite
+ *
+ * Please review the LICENSE file distributed with this software for full
+ * copyright information.
+ */
+
+namespace Adventure;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Parser class tests
+ *
+ * @author    jorgetite
+ * @since     3/31/17
+ * @version   1.0
+ * @copyright Copyright (c) 2017 Jorge Tite
+ */
+class ParserTest extends TestCase
+{
+
+    /**
+     * Tests the parser constructor
+     * @return Parser
+     */
+    public function testNewParser() : Parser
+    {
+        $cm = new CommandMap();
+
+        $command = $this->getMockForAbstractClass(Command::class);
+        $cm->addCommand("test1", $command);
+        $cm->addCommand("test2", $command);
+        $cm->addCommand("test3", $command);
+
+        $parser = new Parser($cm);
+        $this->assertEquals(3, $parser->getCommandWords()->count());
+
+        return $parser;
+    }
+
+    /**
+     * tests matchCommand method
+     * @param Parser $parser
+     * @depends testNewParser
+     */
+    public function testMatchCommand(Parser $parser) : void
+    {
+        $command = $parser->matchCommand("test1");
+
+        $this->assertNotNull($command);
+        $this->assertInstanceOf(Command::class, $command);
+    }
+
+    /**
+     * tests matchCommand method with input does not have a match
+     * @param Parser $parser
+     * @depends testNewParser
+     */
+    public function testMatchCommandNoMatch(Parser $parser) : void
+    {
+        $command = $parser->matchCommand("other");
+
+        $this->assertNull($command);
+    }
+
+    /**
+     * tests matchCommand method with empty input
+     * @param Parser $parser
+     * @depends testNewParser
+     */
+    public function testMatchCommandEmptyInput(Parser $parser) : void
+    {
+        $command = $parser->matchCommand("");
+
+        $this->assertNull($command);
+    }
+
+    /**
+     * tests matchCommand method, input with arguments
+     * @param Parser $parser
+     * @depends testNewParser
+     */
+    public function testMatchCommandInputWithArguments(Parser $parser) : void
+    {
+        $command = $parser->matchCommand("test1 arg1 arg2 arg3");
+
+        $this->assertNotNull($command);
+        $this->assertInstanceOf(Command::class, $command);
+        $this->assertEquals("arg1 arg2 arg3", $command->getArguments());
+    }
+
+    /**
+     * test getCommandWords method
+     * @param Parser $parser
+     * @depends testNewParser
+     */
+    public function testGetCommandWords(Parser $parser) : void
+    {
+        $cw = $parser->getCommandWords();
+
+        $this->assertEquals(3, $cw->count());
+        $this->assertTrue($cw->contains("test1"));
+        $this->assertTrue($cw->contains("test2"));
+        $this->assertTrue($cw->contains("test3"));
+    }
+}
