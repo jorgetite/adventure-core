42d5b0a34b200052d0fb0cbe55b3ad3a8cbfe8e2
Jorge Tite <jorgetite@me.com>
2017-04-14 22:29:40 -0400

    Implemented HelpCommand

--- src/adventure/Command/Help.php
+++ src/adventure/Command/Help.php
@@ -1,15 +1,59 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: jorgetite
- * Date: 4/14/17
- * Time: 9:54 PM
+/*
+ * Copyright © 2017 Jorge Tite
+ *
+ * Please review the LICENSE file distributed with this software for full
+ * copyright information.
  */
+namespace Adventure\Command;
 
-namespace adventure\Command;
+use Adventure\Character;
+use Adventure\Command;
+use Adventure\CommandMap;
 
-
-class Help
+/**
+ * The Help command is used to provide instructions to the player.
+ *
+ * This class is based on the "World of Zuul" framework for writing very
+ * simple text-based adventure games.
+ *
+ * @author    jorgetite
+ * @since     2017-04-14
+ * @version   1.0
+ * @copyright Copyright (c) 2017 Jorge Tite
+ */
+class Help extends Command
 {
+    /**
+     * @var Set
+     *      the commands valid for the adventure game
+     */
+    private $cmdMap;
 
+    /**
+     * Help command constructor.
+     *
+     * @param CommandMap $cmdMap
+     *        the list of valid commands
+     */
+    public function __construct(CommandMap $cmdMap)
+    {
+        $this->cmdMap = $cmdMap;
+    }
+
+    /**
+     * Provides the list of available commands to the player.
+     *
+     * @param Character $character
+     *        the game's character
+     *
+     * @return string
+     *         a result message
+     */
+    public function execute(Character $character) : string
+    {
+        $words = $this->cmdMap->getCommandWords()->toArray();
+
+        return "The command words are: " . implode(", ", $words);
+    }
 }
--- tests/adventure/Command/HelpTest.php
+++ tests/adventure/Command/HelpTest.php
@@ -1,17 +1,43 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: jorgetite
- * Date: 4/14/17
- * Time: 10:14 PM
+/*
+ * Copyright © 2017 Jorge Tite
+ *
+ * Please review the LICENSE file distributed with this software for full
+ * copyright information.
  */
 
-namespace adventure\Command;
+namespace Adventure\Command;
 
-use Adventure\Command\Help;
+use Adventure\Character;
+use Adventure\CommandMap;
 use PHPUnit\Framework\TestCase;
 
+/**
+ * Help command tests
+ *
+ * @author    jorgetite
+ * @since     2017-04-10
+ * @version   1.0
+ * @copyright Copyright (c) 2017 Jorge Tite
+ */
 class HelpTest extends TestCase
 {
+    /**
+     * Tests execute method
+     */
+    public function testExecute() : void
+    {
+        $character = new Character();
+        $cm = new CommandMap();
 
+        $quit = new Quit();
+        $help = new Help($cm);
+
+        $cm->addCommand("quit", $quit);
+        $cm->addCommand("exit", $quit);
+        $cm->addCommand("help", $help);
+
+        $msg = $help->execute($character);
+        $this->assertEquals("The command words are: quit, exit, help", $msg);
+    }
 }
